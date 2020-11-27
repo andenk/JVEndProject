fd95fe0eeb36c87ca33c0e3310e01dd114722920
andreas nehl <andenk@hotmail.se>
2020-11-27 10:54:46 +0100

    Contact

---
+++ JVSlutProject/src/test/java/org/example/ContactTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class ContactTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
