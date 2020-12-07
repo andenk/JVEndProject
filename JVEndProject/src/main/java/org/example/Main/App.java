3983653a6d6b92779e46d03cdfa0bdd33d605a81
andreas nehl <andenk@hotmail.se>
2020-12-07 11:17:04 +0100

    add

--- JVEndProject/src/main/java/org/example/Main/App.java
+++ JVEndProject/src/main/java/org/example/Main/App.java
@@ -1,15 +1,54 @@
 package org.example.Main;
 
 /**
- * Hello world!
- *
- */
-public class App 
+ *This is leone turf
+ **/
+
+import java.util.Scanner;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
 
+        boolean run = true;
+        Scanner myObj = new Scanner(System.in);
 
+        System.out.println("testtest");
+
+        System.out.println(
+                "1: Visa kontakter\n" +
+                        "2: Sök bland kontakter\n" +
+                        "3: Lägg till kontakt\n" +
+                        "4: Ta bort kontakt\n" +
+                        "0: Avsuta\n");
+
+        while(run) {
+            int userChoice = myObj.nextInt();
+            myObj.nextLine();
+            switch (userChoice) {
+                case 0:
+                    run = false;
+                    break;
+                case 1:
+                    //listContactsMethod();
+                    System.out.println("listContactsMethod();");
+                    break;
+                case 2:
+                    //searchContactsMethod();
+                    System.out.println("searchContactsMethod();");
+                    break;
+                case 3:
+                    //addContactMethod();
+                    System.out.println("addContactMethod();");
+                    break;
+                case 4:
+                    //removeContactMethod();
+                    System.out.println("removeContactMethod();");
+                    break;
+                default:
+                    System.out.println("Prova igen");
+            }
+        }
     }
+
 }
