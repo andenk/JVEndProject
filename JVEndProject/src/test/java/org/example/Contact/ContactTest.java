a413ab25af02ad52efdbc3819cf2362afde56d18
andreas nehl <andenk@hotmail.se>
2020-12-08 18:49:18 +0100

    JunitContact

--- JVEndProject/src/test/java/org/example/Contact/ContactTest.java
+++ JVEndProject/src/test/java/org/example/Contact/ContactTest.java
@@ -1,29 +1,38 @@
 package org.example.Contact;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import org.example.Contact.Contact;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class ContactTest {
-    Contact contact = new Contact();
+
+    Contact contact1 = new Contact("David", "070");
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void setName()
     {
-        assertTrue( true );
+        contact1.setName("Erik");
+        assertEquals("Erik",contact1.getName());
 
 
     }
+    @Test
+    public void setPhoneNumber(){
+        contact1.setPhoneNumber("073");
+        assertEquals("073",contact1.getPhoneNumber());
+}
 
     @Test
-    public void getPhoneNumber()
-    {
-
-        assertNotNull(contact.getPhoneNumber());
+    public void getPhoneNumber() {
+        assertEquals("070",contact1.getPhoneNumber());
     }
 
-
+    @Test
+    public void getName() {
+        assertEquals("David",contact1.getName());
+        assertNotEquals("Andreas",contact1.getName());
+    }
 
 
 }
