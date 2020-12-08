149fe7144fc7f5b28ffff2a13048b7e5749ec52e
andreas nehl <andenk@hotmail.se>
2020-12-08 18:57:50 +0100

    JunitContact

--- JVEndProject/src/test/java/org/example/Contact/ContactTest.java
+++ JVEndProject/src/test/java/org/example/Contact/ContactTest.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ *  objektet har name David och nummer 070
+ *  setName ändrar objektets namn till Erik och tittar om objektets namn är uppdaterat till Erik
+ *  set PhoneNumber ändrar objektets nummer till 073 och tittar om objektets nummer är uppdaterat till 073 och att nummret fortfarande inte är 070
+ *  getPhoneNumber() hämta nummret från objektet och titta om det är 070
+ *  getName kolla att name är David
+ *
+ */
 public class ContactTest {
 
     Contact contact1 = new Contact("David", "070");
@@ -21,6 +29,7 @@ public class ContactTest {
     public void setPhoneNumber(){
         contact1.setPhoneNumber("073");
         assertEquals("073",contact1.getPhoneNumber());
+        assertNotEquals("070",contact1.getPhoneNumber());
 }
 
     @Test
@@ -31,7 +40,7 @@ public class ContactTest {
     @Test
     public void getName() {
         assertEquals("David",contact1.getName());
-        assertNotEquals("Andreas",contact1.getName());
+
     }
 
 
